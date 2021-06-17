0cac43c7acc957fac42d507d1a4a56478938f2a3
Ben Lubar <ben.lubar@gmail.com>
2021-06-17 17:32:38 -0500

    clean up error handling, add DecodeOpusFloat

--- opus.go
+++ opus.go
@@ -1,3 +1,4 @@
+// Package opus is a thin wrapper around the opusfile library.
 package opus
 
 // #cgo CFLAGS: -isystem opusfile-0.12/include -I opusfile-0.12/include -isystem opus-1.3.1/include -I opus-1.3.1/include -I opus-1.3.1/celt -I opus-1.3.1/silk
@@ -8,37 +9,36 @@ package opus
 import "C"
 import (
 	"runtime"
+	"strconv"
 	"unsafe"
-
-	"golang.org/x/xerrors"
 )
 
-func DecodeOpus(b []byte) ([]byte, int, error) {
+// DecodeOpus decodes an ogg/opus file in memory to 48kHz 16-bit PCM.
+func DecodeOpus(b []byte) (pcm []byte, numChannels int, err error) {
 	defer runtime.KeepAlive(b)
 
 	var err C.int
 
 	f := C.op_open_memory((*C.uchar)(&b[0]), C.size_t(len(b)), &err)
 	if f == nil {
-		return nil, 0, xerrors.Errorf("opus: native error number %d", int(err))
+		return nil, 0, opusError(err)
 	}
 	defer C.op_free(f)
 
 	var (
-		pcm    []byte
 		pcmBuf [5760 * 2]C.opus_int16
 		link   C.int
 	)
 
 	ret := C.op_read(f, &pcmBuf[0], C.int(len(pcmBuf)), &link)
 	if ret < 0 {
-		return nil, 0, xerrors.Errorf("opus: native error %d", int(ret))
+		return nil, 0, opusError(err)
 	}
 
-	numChannels := int(C.op_head(f, link).channel_count)
+	numChannels = int(C.op_head(f, link).channel_count)
 
 	for ret != 0 {
-		unsafePCMBuf := (*[len(pcmBuf) * 2]byte)(unsafe.Pointer(&pcmBuf[0]))
+		unsafePCMBuf := (*[unsafe.Sizeof(pcmBuf)]byte)(unsafe.Pointer(&pcmBuf[0]))
 		pcm = append(pcm, unsafePCMBuf[:int(ret)*numChannels*2]...)
 		runtime.KeepAlive(pcmBuf)
 
@@ -46,8 +46,78 @@ func DecodeOpus(b []byte) ([]byte, int, error) {
 	}
 
 	if ret < 0 {
-		return nil, 0, xerrors.Errorf("opus: native error %d", int(ret))
+		return nil, 0, opusError(err)
 	}
 
 	return pcm, numChannels, nil
+}
+
+// DecodeOpusFloat decodes an ogg/opus file in memory to 48kHz 32-bit float PCM.
+func DecodeOpusFloat(b []byte) (pcm []byte, numChannels int, err error) {
+	defer runtime.KeepAlive(b)
+
+	var err C.int
+
+	f := C.op_open_memory((*C.uchar)(&b[0]), C.size_t(len(b)), &err)
+	if f == nil {
+		return nil, 0, opusError(err)
+	}
+	defer C.op_free(f)
+
+	var (
+		pcmBuf [5760 * 2]C.float
+		link   C.int
+	)
+
+	ret := C.op_read_float(f, &pcmBuf[0], C.int(len(pcmBuf)), &link)
+	if ret < 0 {
+		return nil, 0, opusError(err)
+	}
+
+	numChannels = int(C.op_head(f, link).channel_count)
+
+	for ret != 0 {
+		unsafePCMBuf := (*[unsafe.Sizeof(pcmBuf)]byte)(unsafe.Pointer(&pcmBuf[0]))
+		pcm = append(pcm, unsafePCMBuf[:int(ret)*numChannels*2]...)
+		runtime.KeepAlive(pcmBuf)
+
+		ret = C.op_read_float(f, &pcmBuf[0], C.int(len(pcmBuf)), &link)
+	}
+
+	if ret < 0 {
+		return nil, 0, opusError(err)
+	}
+
+	return pcm, numChannels, nil
+}
+
+type opusError C.int
+
+func (err opusError) Error() string {
+	switch C.int(err) {
+	case C.OP_EREAD:
+		return "opus: io error"
+	case C.OP_EFAULT:
+		return "opus: internal error"
+	case C.OP_EIMPL:
+		return "opus: stream uses unsupported features"
+	case C.OP_EINVAL:
+		return "opus: invalid parameter"
+	case C.OP_ENOTFORMAT:
+		return "opus: not a valid OGG file"
+	case C.OP_EBADHEADER:
+		return "opus: malformed header"
+	case C.OP_EVERSION:
+		return "opus: unrecognized version number"
+	case C.OP_EBADPACKET:
+		return "opus: bad packet"
+	case C.OP_EBADLINK:
+		return "opus: malformed bitstream"
+	case C.OP_ENOSEEK:
+		return "opus: unable to seek"
+	case C.OP_EBADTIMESTAMP:
+		return "opus: invalid timestamp"
+	default:
+		return "opus: unknown native error code " + strconv.Itoa(int(err))
+	}
 }
